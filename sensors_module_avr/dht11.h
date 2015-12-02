82a9d51bb55076a956892586fa1b778aed82edc9
Augusto Lopez Dantas <aldantas@protonmail.com>
2015-12-02 03:20:23 -0200

    Sensors Module AVR

---
+++ sensors_module_avr/dht11.h
@@ -0,0 +1,29 @@
+/*
+DHT11 Library 0x01
+
+copyright (c) Davide Gironi, 2011
+
+Released under GPLv3.
+Please refer to LICENSE file for licensing information.
+
+References:
+  - DHT-11 Library, by Charalampos Andrianakis on 18/12/11
+
+*/
+
+
+#ifndef DHT11_H_
+#define DHT11_H_
+
+#include <avr/io.h>
+
+//setup parameters
+#define DHT11_DDR DDRC
+#define DHT11_PORT PORTC
+#define DHT11_PIN PINC
+#define DHT11_INPUTPIN PC1
+
+extern int8_t DHT11_get_temperature(void);
+extern int8_t DHT11_get_humidity(void);
+
+#endif
---
+++ sensors_module_avr/sensors_module.c
@@ -0,0 +1,80 @@
+/* Sensors Monitoring API */
+
+/* Copyright (C) 2015  Augusto Lopez Dantas aldantas@protonmail.com */
+
+/* This program is free software; you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License along */
+/* with this program; if not, write to the Free Software Foundation, Inc., */
+/* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avrutil/usart.h>
+#include "dht11.h"
+
+#define LIGHT 'l'
+#define TEMPERATURE 't'
+#define HUMIDTY 'h'
+
+uint8_t volatile sensor;
+uint8_t volatile dht11_value;
+int8_t volatile ret;
+
+ISR(USART_RX_vect)
+{
+	sensor = USART_receive_byte();
+	switch (sensor) {
+	case LIGHT:
+		ADCSRA |= (1 << ADSC);
+		break;
+	case TEMPERATURE:
+		do {
+			ret = DHT11_get_temperature();
+		} while(ret == -1);
+		dht11_value = ret;
+		USART_print_byte(dht11_value);
+		break;
+	case HUMIDTY:
+		do {
+			ret = DHT11_get_humidity();
+		} while(ret == -1);
+		dht11_value = ret;
+		USART_print_byte(dht11_value);
+		break;
+	}
+}
+
+ISR(ADC_vect)
+{
+	uint8_t high, low;
+	low = ADCL;
+	high = ADCH;
+	uint16_t value = high;
+	value = (value << 8) | low;
+	USART_print_word(value);
+}
+
+int main(void)
+{
+	ADMUX = (1 << REFS0) | (1 << MUX2) | (1 << MUX0);
+	// ADC Prescaler divisor: 64
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADEN) | (1 << ADIE);
+
+	USART_init();
+	USART_enable_rx_interrupt;
+	sei();
+
+	while(1);
+
+	return 0;
+}
